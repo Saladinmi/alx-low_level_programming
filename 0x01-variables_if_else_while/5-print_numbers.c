0612a17757137f176ea656b3dbecd97f8bffb37e
saladinmi <selahudinmifta7@gmail.com>
2023-03-19 07:06:10 -0700

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-A program that print number of base 10
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
