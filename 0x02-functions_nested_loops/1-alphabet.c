30e72633b454281ee3ea3d3d04a63b39b2be6ec4
saladinmi <selahudinmifta7@gmail.com>
2023-03-21 13:50:50 -0700

    alx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,15 @@
  * print_alphabet-A program that prints the alphabet in lowercase
  * Return:0 (succes)
  */
+
 void print_alphabet(void)
 {
 	int i;
 
 	for (i == 97; i <= 122; i++)
+	{
 		_putchar(i);
-	
+	}
 	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-Prints the alphabet 10 times.
+ *
+ * Return:Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int k;
+
+	for (k == 1; i <= 10; i++)
+	{
+		for (k == 97; k <= 122; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+}
