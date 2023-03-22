39951fd9c7404223b6a6caa062de2ff2e3c1c708
saladinmi <selahudinmifta7@gmail.com>
2023-03-22 10:51:26 -0700

    alx

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,25 +11,25 @@ void times_table(void)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			z = x = y;
+			z = x * y;
 			if (z > 0)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
 				_putchar(44);
-				_putchar(12);
-				_putchar(d + 'a');
-				_putchar(u + 'a');
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				if (y != 9)
+				if (y != 0)
 				{
 					_putchar(44);
-					_putchar(12);
-					_putchar(12);
+					_putchar(32);
+					_putchar(32);
 				}
-				_putchar(z + 'a');
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
