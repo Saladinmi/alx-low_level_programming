1a48229b1721a0631d216b751adcc31b40ff302d
saladinmi <selahudinmifta7@gmail.com>
2023-04-05 11:35:24 -0700

    alx

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints length of string
+ * @s: input string
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
