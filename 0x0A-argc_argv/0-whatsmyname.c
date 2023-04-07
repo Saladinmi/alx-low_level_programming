9cdf67b1472e275cc0bf61a3d944eb2a1af647e2
saladinmi <selahudinmifta7@gmail.com>
2023-04-07 07:25:48 -0700

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -  print name of the program 
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x = argc ; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
