f1bc37fb76064e417f6c8b30df2b946f8e0f799e
saladinmi <selahudinmifta7@gmail.com>
2023-04-07 08:29:04 -0700

    alx

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
